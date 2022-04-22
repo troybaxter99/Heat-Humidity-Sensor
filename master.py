618b995a79e10fa062a5dffee1f42102185e9666
FGS <fgs22.tamu@gmail.com>
2022-04-22 16:34:23 -0500

    Finished Temperature Sensors

--- master.py
+++ master.py
@@ -3,12 +3,14 @@
 import sht30
 import sys
 import time
+import board
 
 sys.path.insert(1, "/home/pi/Audio-Alert-System/Code")
 import audio
 
 # Create AUDIO and SHT30 Sensor objects
-sensor = sht30.SHT30()
+i2c = board.I2C()
+sensor = sht30.SHT31D(i2c)
 sound = audio.AUDIO("/home/pi/Audio-Alert-System/Audio-Files/Alert-Audio/")
 sound.set_espeak_gap(1000) # Set gap between each word to be 1 second (1000 ms)
 
@@ -22,11 +24,9 @@ def getMeasurements():
     global humidity
     global temp
     
-    sensor.measure() # measures values
-    
     # Get humidity and temperature values from measurement
-    humidity = sensor.get_Humidity()
-    temp = sensor.get_Temp_C()
+    humidity = sensor.relative_humidity
+    temp = sensor.temperature
 
 
 # OVERHEATING Alert System!!!
@@ -34,6 +34,7 @@ def overheating():
     # Play Overheating Alert 3 Times
     for i in range(0,3):
         sound.play_audio("Overheating-Alert.wav")
+        time.sleep(1)
         sound.play_message("Danger! Danger! Danger!")
         sound.play_message("Over Heating! Over Heating! Over Heating!")
     
@@ -42,6 +43,7 @@ def tooHumid():
     # Play Humidity Alert 3 Times
    for i in range(0,3):
         sound.play_audio("Humidity-Alert.wav")
+        time.sleep(2)
         
         # Play message 3 teams
         for j in range(0,3):
--- test-temperature-sensor.py
+++ test-temperature-sensor.py
@@ -1,26 +1,40 @@
 # This program is a test to check if the robot is in danger of overheating or if the internal humidity is too high
-import sht30
+# import sht30
 import sys
 import time
+import board
+import sht30
+
 
 sys.path.insert(1, "/home/pi/Audio-Alert-System/Code")
 import audio
 
-sensor = sht30.SHT30()
-sound = audio.AUDIO()
+i2c = board.I2C()
+sensor = sht30.SHT31D(i2c)
+sound = audio.AUDIO("/home/pi/Audio-Alert-System/Audio-Files/Alert-Audio/")
 sound.set_espeak_gap(1000)
 
-sensor.measure()
 
-humidity = sensor.get_Humidity()
-cTemp = sensor.get_Temp_C()
-fTemp = sensor.get_Temp_F()
+
+humidity = sensor.relative_humidity
+cTemp = sensor.temperature
+fTemp = cTemp * 1.8 + 32
 
 #Output data to screen
 print("Relative Humidity : %.2f %%RH" %humidity)
 print("Temperature in Celsius : %.2f C" %cTemp)
 print("Temperature in Fahrenheit : %.2f F" %fTemp)
 
+humidity = sensor.relative_humidity
+cTemp = sensor.temperature
+fTemp = cTemp * 1.8 + 32
+
+#Output data to screen
+print("Relative Humidity : %.2f %%RH" %humidity)
+print("Temperature in Celsius : %.2f C" %cTemp)
+print("Temperature in Fahrenheit : %.2f F" %fTemp)
+
+'''
 if (cTemp > 25):
     for i in range(1,4):
         sound.play_audio("Overheating-Alert.wav")
@@ -30,6 +44,7 @@ if (cTemp > 25):
 if (humidity>50):
     for i in range(1,4):
         sound.play_audio("Humidity-Alert.wav")
-        
+        ti
         for j in range(1,4):
             sound.play_message("Danger! Too Humid!")
+'''
